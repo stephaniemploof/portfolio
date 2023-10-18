5ec4ba4c3210d5ecac5c02450d3dfed5f7c596e2
stephaniemploof <stephaniemploof@gmail.com>
2023-10-17 20:32:15 -0400

    Latest Update
    My questions:
    
    1: For some reason, adding a grid on the homepage created so much additional space that I can't seem to remove.
    
    2: I believe we discussed in the last class, but I cannot figure out how to have a form send to an email. I will review the last class recording, but if you know off the top of your head, that would be helpful.
    
    3: I know how to access the URL to my github portal, but if you could help me link the URL for additional pages I have to my buttons, that would be helpful!

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello')
+function submitForm() {
+    // Basic form validation
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (firstName === "" || lastName === "" || email === "" || message === "") {
+        alert("All fields are required");
+        return;
+    }
+}
